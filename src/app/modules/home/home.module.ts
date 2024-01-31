317c34a75d1efb63476030bd7cf936d8db21d4a0
flowercordoba <flowercordoba7@gmail.com>
2024-01-31 06:14:57 -0500

    agregando las estrutura de navegacion del home

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HomePageComponent
   ],
   imports: [
     CommonModule,
